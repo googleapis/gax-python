b2c00905dd99837cf9b788a23e9bea6196d2b114
Jon Wayne Parrott <jonwayne@google.com>
2016-11-07 12:47:06 -0800

    Add gRPC authentication using google.auth (#136)

---
+++ google/gax/_grpc_google_auth.py
@@ -0,0 +1,77 @@
+# Copyright 2016, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Provides gRPC authentication support using google.auth."""
+
+from __future__ import absolute_import
+
+import google.auth
+import google.auth.credentials
+import google.auth.transport.grpc
+
+try:
+    import google.auth.transport.requests
+    # pylint: disable=invalid-name
+    # Pylint recognizes this as a class, but we're aliasing it as a factory
+    # function.
+    _request_factory = google.auth.transport.requests.Request
+    # pylint: enable=invalid-name
+except ImportError:
+    try:
+        import httplib2
+        import google_auth_httplib2
+
+        def _request_factory():
+            http = httplib2.Http()
+            return google_auth_httplib2.Request(http)
+
+    except ImportError:
+        raise ImportError(
+            'No HTTP transport is available. Please install requests or '
+            'httplib2 and google_auth_httplib2.')
+
+
+def get_default_credentials(scopes):
+    """Gets the Application Default Credentials."""
+    credentials, _ = google.auth.default()
+
+    if scopes is not None:
+        credentials = google.auth.credentials.with_scopes_if_required(
+            credentials, scopes)
+
+    return credentials
+
+
+def secure_authorized_channel(
+        credentials, target, ssl_credentials=None):
+    """Creates a secure authorized gRPC channel."""
+    http_request = _request_factory()
+    return google.auth.transport.grpc.secure_authorized_channel(
+        credentials, target, http_request,
+        ssl_credentials=ssl_credentials)
---
+++ test/test__grpc_google_auth.py
@@ -0,0 +1,119 @@
+# Copyright 2016, Google Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+# copyright notice, this list of conditions and the following disclaimer
+# in the documentation and/or other materials provided with the
+# distribution.
+#     * Neither the name of Google Inc. nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# pylint: disable=missing-docstring,no-self-use,no-init,invalid-name
+# pylint: disable=protected-access
+"""Unit tests for _grpc_google_auth."""
+
+from __future__ import absolute_import
+
+import sys
+import unittest2
+
+import google.auth.credentials
+import google.auth.transport.requests
+from google.gax import _grpc_google_auth
+import google_auth_httplib2
+import mock
+from six.moves import reload_module
+
+
+class TestGetDefaultCredentials(unittest2.TestCase):
+    @mock.patch('google.auth.default')
+    def test(self, default_mock):
+        credentials = mock.Mock(
+            token='token', requires_scopes=True,
+            spec=google.auth.credentials.Scoped)
+        credentials.with_scopes.return_value = credentials
+        default_mock.return_value = credentials, 'project'
+        fake_scopes = ['fake', 'scopes']
+
+        got = _grpc_google_auth.get_default_credentials(fake_scopes)
+
+        self.assertEqual(got, credentials)
+        credentials.with_scopes.assert_called_once_with(fake_scopes)
+
+
+class TestSecureAuthorizedChannel(unittest2.TestCase):
+    @mock.patch('google.gax._grpc_google_auth._request_factory')
+    @mock.patch('google.auth.transport.grpc.secure_authorized_channel')
+    def test(self, secure_authorized_channel_mock, request_factory_mock):
+
+        got_channel = _grpc_google_auth.secure_authorized_channel(
+            mock.sentinel.credentials,
+            mock.sentinel.target)
+
+        request_factory_mock.assert_called_once_with()
+        secure_authorized_channel_mock.assert_called_once_with(
+            mock.sentinel.credentials, mock.sentinel.target,
+            request_factory_mock.return_value, ssl_credentials=None)
+
+        self.assertEqual(
+            got_channel, secure_authorized_channel_mock.return_value)
+
+
+class TestRequestsRequestFactory(unittest2.TestCase):
+    def test(self):
+        self.assertEqual(
+            _grpc_google_auth._request_factory,
+            google.auth.transport.requests.Request)
+
+
+class TestHttplib2RequestFactory(unittest2.TestCase):
+    def setUp(self):
+        # Block requests module during this test.
+        self._requests_module = sys.modules['google.auth.transport.requests']
+        sys.modules['google.auth.transport.requests'] = None
+        reload_module(_grpc_google_auth)
+
+    def tearDown(self):
+        sys.modules['google.auth.transport.requests'] = self._requests_module
+        reload_module(_grpc_google_auth)
+
+    def test(self):
+        request = _grpc_google_auth._request_factory()
+        self.assertIsInstance(request, google_auth_httplib2.Request)
+
+
+class TestNoHttpClient(unittest2.TestCase):
+    def setUp(self):
+        # Block all transport modules during this test.
+        self._requests_module = sys.modules['google.auth.transport.requests']
+        self._httplib2_module = sys.modules['google_auth_httplib2']
+        sys.modules['google.auth.transport.requests'] = None
+        sys.modules['google_auth_httplib2'] = None
+
+    def tearDown(self):
+        sys.modules['google.auth.transport.requests'] = self._requests_module
+        sys.modules['google_auth_httplib2'] = self._httplib2_module
+        reload_module(_grpc_google_auth)
+
+    def test(self):
+        with self.assertRaises(ImportError):
+            reload_module(_grpc_google_auth)
